5382a473c004f55505feba3389feda6fb9f518bd
Raven Iqqe <raviqqe@gmail.com>
2016-05-06 00:41:21 +0900

    Create character indices

--- json2array.py
+++ json2array.py
@@ -27,32 +27,47 @@ def array(sequence):
   return numpy.array(list(sequence), dtype=CHAR_DATATYPE)
 
 
-## word indices
+def documents_to_words(documents):
+  return {word
+          for document in documents
+          for sentence in document
+          for word in sentence}
+
+
+def documents_to_chars(documents):
+  return {char for word in documents_to_words(documents) for char in word}
+
+
+def index_elems_in_set(elems):
+  return {elem : index for index, elem in enumerate(sorted(elems))}
+
+
+## indices
+
+def create_char_indices(documents):
+  return index_elems_in_set(documents_to_chars(documents)
+                            | {NULL_CHAR, UNKNOWN_CHAR})
+
 
 def create_word_indices(documents):
-  words = {word
-           for document in documents
-           for sentence in document
-           for word in sentence} \
-          | {NULL_WORD, UNKNOWN_WORD}
-
-  return {word : index for index, word in enumerate(sorted(words))}
+  return index_elems_in_set(documents_to_words(documents)
+                            | {NULL_WORD, UNKNOWN_WORD})
 
 
 ## word array
 
-def create_word_array(word_indices, word_length):
+def create_word_array(word_indices, word_length, char_indices):
   word_array = numpy.zeros((len(word_indices), word_length),
                            dtype=CHAR_DATATYPE)
 
   for word, index in word_indices.items():
-    word_array[index] = word_to_array(word, word_length)
+    word_array[index] = word_to_array(word, word_length, char_indices)
 
   return word_array
 
 
-def word_to_array(word, word_length):
-  return array(ord(char) for char in format_word(word, word_length))
+def word_to_array(word, word_length, char_indices):
+  return array(char_indices[char] for char in format_word(word, word_length))
 
 
 def format_word(word, word_length):
@@ -60,9 +75,11 @@ def format_word(word, word_length):
          word + NULL_CHAR * (word_length - len(word))
 
 
-def save_word_array(filename, word_indices, *, word_length):
+def save_word_array(filename, word_indices, *, word_length, char_indices):
   if filename is not None:
-    create_word_array(word_indices, word_length=word_length).dump(filename)
+    create_word_array(word_indices,
+                      word_length=word_length,
+                      char_indices=char_indices).dump(filename)
 
 
 ## document array
@@ -155,11 +172,13 @@ def main():
 
   documents = json.loads(json_documents)
 
+  char_indices = create_char_indices(documents)
   word_indices = create_word_indices(documents)
 
   save_word_array(args.word_array_file,
                   word_indices,
-                  word_length=args.word_length)
+                  word_length=args.word_length,
+                  char_indices=char_indices)
 
   save_document_array(args.document_array_file,
                       documents,
