bde9d2973bfec67fa944c5f1e4bf62296e7cb1b0
Yota Toyama <raviqqe@gmail.com>
2016-06-15 11:39:28 +0900

    Make prototype of multi-language version

--- tokeniz.py
+++ tokeniz.py
@@ -9,13 +9,31 @@ import sys
 
 
 
-def tokenize_document(document):
-  return [nltk.tokenize.word_tokenize(sentence)
-          for sentence in nltk.tokenize.sent_tokenize(document)]
+class DocumentTokenizer:
+  def __init__(self, language):
+    self._language = language
+
+  def tokenize(self, document):
+    return {
+      "english" : self._tokenize_in_english,
+      "japanese": self._tokenize_in_japanese,
+    }[self._language](document)
+
+  def _tokenize_in_english(self, document):
+    return [nltk.tokenize.word_tokenize(sentence)
+            for sentence in nltk.tokenize.sent_tokenize(document)]
+
+  def _tokenize_in_japanese(self, document):
+    return
 
 
-def tokenize_documents(documents):
-  return multiprocessing.Pool().map(tokenize_document, documents)
+def tokenize_documents(documents, language):
+  return multiprocessing.Pool().map(DocumentTokenizer(language).tokenize,
+                                    documents)
+
+
+def print_as_json(obj):
+  print(json.dumps(obj, indent="\t"))
 
 
 def get_args():
@@ -23,13 +41,14 @@ def get_args():
   arg_parser.add_argument("document_file",
                           type=argparse.FileType(),
                           default=sys.stdin)
+  arg_parser.add_argument("-l", "--language", default="english")
   return arg_parser.parse_args()
 
 
 def main():
   args = get_args()
-  print(json.dumps(tokenize_documents(args.document_file.readlines()),
-                   indent="\t"))
+  print_as_json(tokenize_documents(args.document_file.readlines(),
+                                   language=args.language))
 
 
 if __name__ == "__main__":
